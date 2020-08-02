fc8d570bc45b977d16f2ced84b875ae5c395ec4c
raul <rauldionisiosh@gmail.com>
2020-08-02 19:39:54 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain'
+import Carrousel from './components/Carousel'
+import Footer from './components/Footer'
+import dadosIniciais from './data/dados_iniciais.json'
+
+
+function App() {
+  return (
+   <div style={{background:"#141414"}}>
+      <Menu />
+      
+      <BannerMain
+         videoTitle ={dadosIniciais.categorias[0].videos[0].titulo} 
+         url={dadosIniciais.categorias[0].videos[0].url}
+         videoDescription={`Guitarra Rock`}
+         />
+
+      <Carrousel
+       ignoreFirstVideo
+       category={dadosIniciais.categorias[0]}
+       />
+
+      <Carrousel category={dadosIniciais.categorias[1]} />
+
+      <Carrousel category={dadosIniciais.categorias[2]}/>
+
+      <Carrousel category={dadosIniciais.categorias[3]}/>
+
+      <Footer />
+   </div>
+  );
+}
+
+export default App;
