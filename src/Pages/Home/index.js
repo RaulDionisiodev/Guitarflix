e4b55e8ae5033944ee9f3372a7f4c9a9affad57c
raul <rauldionisiosh@gmail.com>
2020-08-08 20:10:39 -0300

    Roteamento, melhorias no carrossel e início da página de cadastro

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain'
-import Carrousel from './components/Carousel'
-import Footer from './components/Footer'
-import dadosIniciais from './data/dados_iniciais.json'
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain'
+import Carrousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
+import dadosIniciais from '../../data/dados_iniciais.json'
 
 
-function App() {
+function Home() {
   return (
    <div style={{background:"#141414"}}>
       <Menu />
@@ -38,4 +38,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+
+import CadastroVideo from './Pages/Cadastro/Video'
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import CadastroCategoria from './Pages/Cadastro/Categoria';
+
+const pagina404 = () => (<div>Ops! Página não encontrada</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} />
+        <Route component={pagina404} />
+      </Switch>
+      
+    </BrowserRouter>,
   document.getElementById('root')
-);
-;
+  );
